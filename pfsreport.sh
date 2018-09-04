615d33832a6d6c9f1577ffccff42d0b1aed33cfc
BenedictusAryo <benedictusaryo@outlook.com>
2018-09-04 17:43:19 +0700

    Create Query Command in one line

--- pfsreport.sh
+++ pfsreport.sh
@@ -2,10 +2,10 @@
 
 # The Script is purposed to get PFS Report for Port Utilization and Total Error
 
-psql -U postgres -d pfsdb
-echo "365Pass"
-
-
-\copy (select name as PortName, date_trunc('hour',to_timestamp(utilization2.timestamp)) as HourlyTimestamp, trunc(avg(current_utilization/10.0),1) as Utilization, sum(current_errors) as TotalError , sum(congestion_errors) as CongestionError from subport,utilization2 where record_id=source_id group by PortName, HourlyTimestamp order by HourlyTimestamp,name) To 'pfsdbtest.csv' With CSV HEADER DELIMITER',';
+psql -U postgres -d ucmsserverdb_3_6_6_11 -c "COPY(select name as PortName, 
+date_trunc('hour',to_timestamp(utilization2.timestamp)) as HourlyTimestamp, 
+trunc(avg(current_utilization/10.0),1) as Utilization, sum(current_errors) as TotalError , sum(congestion_errors) 
+as CongestionError from subport,utilization2 where record_id=source_id group by PortName, HourlyTimestamp order 
+by HourlyTimestamp,name) To '/tmp/jktpfs1dbtest.csv' With CSV HEADER DELIMITER',';"
 
 
