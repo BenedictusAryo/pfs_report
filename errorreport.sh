9e30a23992314b7c29cec2c9ec6194c83d1aef07
Benedictus Aryo <benedictus.aryo@myoffice365.site>
2018-09-14 12:50:48 +0700

    Fixing Date Formatting

--- errorreport.sh
+++ errorreport.sh
@@ -19,11 +19,7 @@ YY=$(date +"%Y")
 echo "Script is running . . . "
 
 psql -U postgres -d ucmsserverdb_3_6_6_11 -c "COPY(select name as PortName, 
-<<<<<<< HEAD:pfsreport.sh
 to_char(date_trunc('hour',to_timestamp(utilization2.timestamp)+'7 hour'::interval), 'yyyy/MM/dd HH24:MI') as HourlyTimestamp, 
-=======
-date_trunc('hour',to_timestamp(utilization2.timestamp)+'8 hour'::interval) as HourlyTimestamp, 
->>>>>>> c2fea30f2178f06feadae954099c8ad974c003f6:errorreport.sh
 trunc(avg(current_utilization/10.0),1) as Utilization, sum(current_errors) as TotalError , sum(congestion_errors) 
 as CongestionError from subport,utilization2 where record_id=source_id group by PortName, HourlyTimestamp order 
 by HourlyTimestamp,name) To '/tmp/pfsdbtest_report.csv' With CSV HEADER DELIMITER',';"
